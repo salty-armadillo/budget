7eb73c76202c01fa0841c11d1b97f6f77504cecf
Kaman Chan <kamanchan27@gmail.com>
2021-01-13 22:58:58 +1100

    fetch latest budget for given timeframe + basic display

--- budget-frontend/src/pages/BudgetsPage/BudgetsPage.js
+++ budget-frontend/src/pages/BudgetsPage/BudgetsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import axios from 'axios';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -8,6 +9,8 @@ import { Banner } from '../../components/Banner';
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 const styles = (theme) => ({
     overlap: {
@@ -22,6 +25,11 @@ const styles = (theme) => ({
     paper: {
         width: '90%',
         margin: 'auto'
+    },
+    title: {
+        color: theme.palette.secondary.dark,
+        textAlign: 'left',
+        padding: '1rem'
     }
 })
 
@@ -30,7 +38,13 @@ export class BudgetsPage extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.length = 1;
+
+        this.state = {
+            timeframe: "month",
+            offset: 0,
+            data: []
+        }
     }
 
     componentDidMount() {
@@ -38,6 +52,25 @@ export class BudgetsPage extends React.Component {
     }
 
     getBudgetData = () => {
+        const { timeframe } = this.state;
+
+        const start = moment().startOf(timeframe).format('YYYY-MM-DD HH:mm:ss');
+        const end = moment().endOf(timeframe).format('YYYY-MM-DD HH:mm:ss');
+
+        const url = `http://localhost:5000/budgetgoals/fetch?timeframe=${timeframe}&start=${start}&end=${end}`;
+
+        return (
+            axios
+                .get(url)
+                .then((response) => {
+                    this.setState({
+                        data: JSON.parse(response.data && response.data[0] && response.data[0].goals)
+                    })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        )
 
     }
 
@@ -53,7 +86,18 @@ export class BudgetsPage extends React.Component {
                 <Grid className={classes.overlap} container />
                 <Grid className={classes.paperGrid}>
                     <Paper className={classes.paper}>
-                        
+                        <Typography className={classes.title} variant='h6'>Budget</Typography>
+                        <Divider/>
+                        <Grid container spacing={2}>
+                            { Object.keys(data).map((category) => {
+                                return (
+                                    <Grid item container xs={4} direction='column' justify='center' alignContent='center'>
+                                        <Typography variant='h4'>{category}</Typography>
+                                        <Typography>{data[category]}</Typography>
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
                     </Paper>
                 </Grid>
             </React.Fragment>
