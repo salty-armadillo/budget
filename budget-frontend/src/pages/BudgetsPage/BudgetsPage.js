ad8a7a46698b655a9e94d606ed680e3c7f12a2f1
Kaman Chan <kamanchan27@gmail.com>
2021-01-16 23:44:15 +1100

    added indicator for budget categories + styling

--- budget-frontend/src/pages/BudgetsPage/BudgetsPage.js
+++ budget-frontend/src/pages/BudgetsPage/BudgetsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import axios from 'axios';
+import clsx from 'clsx';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -14,6 +15,7 @@ import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Box from '@material-ui/core/Box';
+import WarningIcon from '@material-ui/icons/Warning';
 
 const styles = (theme) => ({
     overlap: {
@@ -27,7 +29,8 @@ const styles = (theme) => ({
     },
     paper: {
         width: '90%',
-        margin: 'auto'
+        margin: 'auto',
+        marginBottom: '2rem'
     },
     title: {
         color: theme.palette.secondary.dark,
@@ -36,9 +39,34 @@ const styles = (theme) => ({
     },
     categoryCard: {
         width: '80%',
-        margin: '2.5rem 1rem 1rem 1rem'
+        margin: '2.5rem 1rem 1rem 1rem',
+        outlineWidth: '0.1rem',
+        outlineStyle: 'solid'
     },
-    box: {}
+    categoryCardError: {
+        outlineColor: theme.palette.error.light
+    },
+    categoryCardWarning: {
+        outlineColor: theme.palette.warning.light
+    },
+    categoryCardSuccess: {
+        outlineColor: theme.palette.success.light
+    },
+    indicator: {
+        position: 'absolute',
+        padding: '0.25rem',
+        marginTop: '-1rem',
+        marginLeft: '0.5rem'
+    },
+    indicatorError: {
+        backgroundColor: theme.palette.error.light
+    },
+    indicatorWarning: {
+        backgroundColor: theme.palette.warning.light
+    },
+    indicatorSuccess: {
+        backgroundColor: theme.palette.success.light
+    }
 })
 
 export class BudgetsPage extends React.Component {
@@ -51,12 +79,14 @@ export class BudgetsPage extends React.Component {
         this.state = {
             timeframe: "month",
             offset: 0,
-            data: []
+            budgetData: [],
+            transactionData: []
         }
     }
 
     componentDidMount() {
         this.getBudgetData();
+        this.getTransactionData();
     }
 
     getBudgetData = () => {
@@ -72,7 +102,7 @@ export class BudgetsPage extends React.Component {
                 .get(url)
                 .then((response) => {
                     this.setState({
-                        data: JSON.parse(response.data && response.data[0] && response.data[0].goals)
+                        budgetData: JSON.parse(response.data && response.data[0] && response.data[0].goals)
                     })
                 })
                 .catch((error) => {
@@ -82,9 +112,54 @@ export class BudgetsPage extends React.Component {
 
     }
 
+    getTransactionData = () => {
+        const { timeframe } = this.state;
+
+        const start = moment().startOf(timeframe).format('YYYY-MM-DD HH:mm:ss');
+        const end = moment().endOf(timeframe).format('YYYY-MM-DD HH:mm:ss');
+
+        const url = `http://localhost:5000/transactions/fetch?start=${start}&end=${end}`;
+
+        return (
+            axios
+                .get(url)
+                .then((response) => {
+                    console.log(response.data)
+                    this.setState({
+                        transactionData: response.data
+                    })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        )
+
+    }
+
+    getCategoryLevel = (category) => {
+        const { budgetData, transactionData } = this.state;
+
+        const budgetValue = budgetData[category];
+        
+        var transactionTotal = 0;
+        transactionData.forEach((t) => {
+            if (t.category === category) {
+                transactionTotal += -1*(t.amount);
+            }
+        })
+
+        if (transactionTotal > budgetValue) {
+            return "error";
+        } else if (transactionTotal > budgetValue*0.75) {
+            return "warning";
+        } else {
+            return "ok"
+        }
+    }
+
     render() {
         const { classes } = this.props;
-        const { data } = this.state;
+        const { budgetData } = this.state;
 
         return (
             <React.Fragment>
@@ -97,12 +172,26 @@ export class BudgetsPage extends React.Component {
                         <Typography className={classes.title} variant='h6'>Budget</Typography>
                         <Divider/>
                         <Grid container spacing={2}>
-                            { Object.keys(data).map((category) => {
+                            { Object.keys(budgetData).map((category) => {
+                                const catLevel = this.getCategoryLevel(category);
                                 return (
                                     <Grid item container xs={6} sm={4}>
-                                        <Card className={classes.categoryCard}>
+                                        <Card square elevation={3} className={clsx(classes.categoryCard, {
+                                                    [classes.categoryCardError]: catLevel === "error",
+                                                    [classes.categoryCardWarning]: catLevel === "warning",
+                                                    [classes.categoryCardSuccess]: catLevel === "ok"
+                                                })}>
+                                            <Box
+                                                className={clsx(classes.indicator, {
+                                                    [classes.indicatorError]: catLevel === "error",
+                                                    [classes.indicatorWarning]: catLevel === "warning",
+                                                    [classes.indicatorSuccess]: catLevel === "ok"
+                                                })}
+                                            >
+                                                <WarningIcon />
+                                            </Box>
                                             <CardContent>
-                                                <Typography variant='h2'>{data[category]}</Typography>
+                                                <Typography variant='h2'>{budgetData[category]}</Typography>
                                                 <Typography>{category}</Typography>
                                             </CardContent>
                                         </Card>
